cc154e1a68f539dc0472a5fd7a758b3b302cf526
AndyofStateFarm <andrewjb97@yahoo.com>
2024-07-10 00:32:09 -0400

    Set up App.js and make components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,2 +1,25 @@
 // U08282838
 
+import React from 'react';
+import './App.css';
+import Gallery from './components/Gallery';
+import { Header, Footer } from './components/Header';
+
+const Team = [
+  {name: 'Ryan Knight', role: 'CEO', photo: 'Ryan.jpg'},
+  {name: 'Tyler Young', role: 'HR Director', photo: 'Tyler.jpg'},
+  {name: 'Mark Darfler', role: 'Finance Director', photo: 'Mark.jpg'},
+
+];
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Meet the Team" />
+      <Gallery Members={Team} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
